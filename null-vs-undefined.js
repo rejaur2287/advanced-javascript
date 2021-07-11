3e90f992839f9d2482e86075e9464799c23f76cd
rejaur <rezaurewubd@gmail.com>
2021-07-11 16:16:12 +0600

    null vs undefined cleared for interview

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,18 +1,18 @@
-// let pakhi;
-// function add(num1, num2) {
-//   console.log(num1 + num2);
-//   return;
-// }
+let pakhi;
+function add(num1, num2) {
+  console.log(num1 + num2);
+  return;
+}
 
-// function add(num1, num2 ) {
-//   console.log(num1, num2);
-// }
+function add(num1, num2) {
+  console.log(num1, num2);
+}
 
-// const premik = { name: 'smart dude', phone: 4567 };
-// console.log(premik.gf);
+const premik = { name: 'smart dude', phone: 4567 };
+console.log(premik.gf);
 
-// let fun = undefined;
-// console.log(fun);
+let fun = undefined;
+console.log(fun);
 
-// let ages = [13, 34, 14, 15];
-// console.log(ages[12]);
+let ages = [13, 34, 14, 15];
+console.log(ages[12]);
